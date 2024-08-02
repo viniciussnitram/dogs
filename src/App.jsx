236f00a7caec3fa3f3aee8a00cdbe82535ddd001
viniciussnitram <vinicius.martins.vsm@outlook.com>
2024-08-02 08:32:25 -0300

    feat: imported the Components 'Header', 'Footer' and 'Home' to the Component App. Using react-router-dom in App and makes a route to Home Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div>
-    <Header />
-    <Footer />
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
