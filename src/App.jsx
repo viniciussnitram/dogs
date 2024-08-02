dafda823d6518a6c49b74f72158abaf7c19ba732
viniciussnitram <vinicius.martins.vsm@outlook.com>
2024-08-02 09:11:24 -0300

    feat: reseted some CSS global styles and imported the App.css into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
