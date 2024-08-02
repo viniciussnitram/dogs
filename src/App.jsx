32974c3c2414f8abcfd02297551dc0caa9421c49
viniciussnitram <vinicius.martins.vsm@outlook.com>
2024-08-02 08:37:05 -0300

    feat: imported and makes a route to the Login Component in App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
